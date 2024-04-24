db14b8f4d54b9c1f1318fc36bd81443decc24db9
Anna <annacrabb92@gmail.com>
2024-04-24 16:32:45 -0400

    new push

---
+++ app.js
@@ -0,0 +1,23 @@
+varFour = 4;
+varEight = 8;
+varFifteen = 15;
+varSixteen = 16;
+varTwentyThree = 23;
+varFortyTwo = 42;
+varOneOhEight = 108;
+varFortySeven = 47;
+varPi = 3.14;
+
+console.log(varFour + varTwentyThree);
+console.log(varEight + varSixteen);
+console.log(varTwentyThree - varFortyTwo);
+console.log(varFortySeven - varTwentyThree);
+console.log(varFour * varTwentyThree);
+console.log(varFortySeven * varPi);
+console.log(varSixteen / varFour);
+console.log(varSixteen / varEight);
+console.log(varOneOhEight ++);
+console.log(varFortySeven ++);
+console.log(varOneOhEight --);
+console.log(varFour --);
+
